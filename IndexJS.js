030ebbd04f442a35b2ae6dcf409325924f01d316
MadaraMik <57146030+MadaraMik@users.noreply.github.com>
2019-11-18 19:32:41 +0100

    Update IndexJS.js

--- IndexJS.js
+++ IndexJS.js
@@ -1,22 +1,22 @@
 function indexJS(){}
 
-exports.ApriMappa = function(){
-    var Map = L.map('mappa').setView(GetCurrentLocat(), 13);
+function apriMappa(){
+    var Map = L.map('mappa').setView(getCurrentLocat(), 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-		maxZoom: 18,
-		id: 'mapbox.streets',
-		accessToken: 'pk.eyJ1IjoicHJvZ2V0dGFudGlzdGkiLCJhIjoiY2syMGticWdtMTExOTNwdWc3ZW9pOW5zMiJ9.qioBe4OljXB2XwJg9vS3Kw'
-	}).addTo(Map);
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery  <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox.streets',
+                accessToken: 'pk.eyJ1IjoicHJvZ2V0dGFudGlzdGkiLCJhIjoiY2syMGticWdtMTExOTNwdWc3ZW9pOW5zMiJ9.qioBe4OljXB2XwJg9vS3Kw'
+        }).addTo(Map);
 }
 
-exports.GetCurrentLocat = function(){
-	if(navigator.geolocation)
-	{
-		navigator.geolocation.getCurrentPosition(function(position)
-		{
-		   var currPosition = new L.LatLng(position.coords.latitude, position.coords.longitude); 
-		})
-	    return currPosition; 	
-	}
+function getCurrentLocat(){
+        var currPosition;
+        if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition(function(position)
+                {
+                   currPosition = new L.LatLng(position.coords.latitude, position.coords.longitude);
+                })
+            return currPosition;
+        }
 }
